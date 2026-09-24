aee6c2a040c9d3b2028c0d5b2d88b41d8ca989ab
Towfique Kabir <towfique.pranto@gmail.com>
2026-09-24 04:19:55 -0400

    feat:Add & Update Main class

--- src/Main.java
+++ src/Main.java
@@ -1,13 +1,43 @@
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
-void main() {
-    //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-    // to see how IntelliJ IDEA suggests fixing it.
-    IO.println(String.format("Hello and welcome!"));
+public class Main {
+    public static void main(String[] args) {
+        Novel novel1 = new Novel("Good Novel", 445, "horror");
+        Novel novel2 = new Novel("Bad Novel", 445, "comedy");
+        Novel novel3 = new Novel("Average Novel", 445, "mystery");
+        Novel novel4 = new Novel("My Novel", 445, "detective");
 
-    for (int i = 1; i <= 5; i++) {
-        //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-        // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-        IO.println("i = " + i);
+        Librarian librarian1 = new Librarian("Pranto");
+        Librarian librarian2 = new Librarian("Shanto");
+
+        Book [] book1 = new Book[4];
+        book1[0] = novel1;
+        book1[1] = novel2;
+        book1[2] = novel3;
+        book1[3] = novel4;
+
+        for (int i = 0; i < 4; i++) {
+            book1[i].displayInfo();
+        }
+
+        librarian1.addNovel(novel1);
+        librarian1.addNovel(novel2);
+        librarian2.addNovel(novel3);
+        librarian2.addNovel(novel4);
+
+        Shelf shelf1 = new Shelf("Wooden");
+        Shelf shelf2 = new Shelf("Steel", 50);
+
+        shelf1.displayShelf();
+        shelf2.displayShelf();
+
+        LoanRecord loanRecord1 = new LoanRecord(novel1, librarian1);
+        LoanRecord loanRecord2 = new LoanRecord(novel3, librarian2);
+        loanRecord1.setFine(40.0);
+        loanRecord2.setFine(-10.0);
+
+        System.out.println(loanRecord1.getRecord());
+        System.out.println(loanRecord2.getRecord());
+
+        librarian1.listNovels();
+        librarian2.listNovels();
     }
 }
